56dcff92a8294c3d4a5d80766d20e0eb02691bca
Calvin Yeung <calyeung92@gmail.com>
2015-06-28 13:08:23 -0400

    RockPaperSci Project FINAL

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -1,59 +1,40 @@
-////////////////////////////////////////////////
-/*   Provided Code - Please Don't Edit   */
-////////////////////////////////////////////////
-'use strict';
-
 function getInput() {
-    console.log("Please choose either 'rock', 'paper', or 'scissors'.")
+    console.log("Please choose either 'Rock', 'Paper', or 'Scissors'.");
     return prompt();
 }
 function randomPlay() {
     var randomNumber = Math.random();
     if (randomNumber < 0.33) {
-        return "rock";
+        return "Rock";
     } else if (randomNumber < 0.66) {
-        return "paper";
+        return "Paper";
     } else {
-        return "scissors";
+        return "Scissors";
     }
 }
-////////////////////////////////////////////////
-/*           Write Your Code Below            */
-////////////////////////////////////////////////
 
 function getPlayerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return move || getInput();
+       return (move || getInput());
 }
 
 function getComputerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return move || randomPlay();
+    return (move || randomPlay());
 }
 
+
+
 function getWinner(playerMove,computerMove) {
-    var winner;
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
-    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
-    if (playerMove === "scissors")
+    if (playerMove === computerMove){
+        winner = "tie";
+    }
+    else if (playerMove === "scissors"){
         if(computerMove ==="rock"){
             winner = "computer";
         }
         else{
             winner = "player";
-
+        }
     }
-    
-    else if (playerMove === computerMove){
-        winner = "tie";
-    }
-    
     else if (playerMove ==="rock"){
         if (computerMove === "paper" ){
             winner = "computer";
@@ -77,26 +58,28 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
-    while (playerWins <5 && computerWins <5){
+
+    while (playerWins < 5 && computerWins < 5){
         var playerMove = getPlayerMove();
         var computerMove = getComputerMove();
         var winner = getWinner(playerMove, computerMove);
         if (winner === "player"){
-            playerWins +=1;
+            playerWins += 1;
+        }else if (winner === "computer"){
+            computerWins += 1;
+        }else if (winner === "tie"){
+            console.log("Tie, try again.");
         }
-        else if (winner === "computer"){
-            computerWins +=1;
-        }
-
-
-    console.log("Score " + [playerWins] +" For Player and " +[computerWins] +" For computer");
-}
-
-console.log("End Score " + [playerWins] +" for Player and " + [computerWins] + " For computer");
-
+   
+        console.log("The score is " + [playerWins] +" for the player and "+[computerWins] +" for the computer" );
+    }
+        
+    console.log("Final score"+ [playerWins]+ "for the player and "+[computerWins]+ " for the computer");
+        
     return [playerWins, computerWins];
 }
+    
+playToFive();
 
-playToFive ();
+
+
